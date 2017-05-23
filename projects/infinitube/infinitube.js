39cc738d91e10a0a852c436e3af804ee0a74fa89
Matt Welsh <mdw@mdw.la>
2017-05-22 22:12:43 -0700

    Added some platforms and physics

--- projects/infinitube/infinitube.js
+++ projects/infinitube/infinitube.js
@@ -1,59 +1,32 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', 
-    { preload: preload, create: create, update: update });
+    { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('background','assets/debug-grid-1920x1920.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('ground', 'assets/platform.png');
 }
 
 var player;
-var platforms;
 var cursors;
-
-var stars;
-var score = 0;
-var scoreText;
+var platforms;
 
 function create() {
 
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
-
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player = game.add.sprite(game.world.centerX, 30, 'dude');
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
     //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
+    player.body.bounce.y = 0.5;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
@@ -61,41 +34,18 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    makePlatforms();
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
-    
+
+    game.camera.follow(player);
+
 }
 
 function update() {
 
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    var hitPlatform = game.physics.arcade.collide(player, platforms);
 
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
@@ -121,22 +71,21 @@ function update() {
 
         player.frame = 4;
     }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
 
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
+function render() {
+  game.debug.cameraInfo(game.camera, 32, 32);
+  game.debug.spriteCoords(player, 32, 500);
+}
 
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+
+function makePlatforms() {
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    for (i = 0; i < 10; i++) {
+      ledge = platforms.create(900, i * 100, 'ground');
+    }
 
 }
